4c0a3d9155d209911554161661544958b76d0923
Nicolas Bernier <nbernier@gmail.com>
2020-05-10 16:33:57 -0400

    config

--- taplist/src/firebase.js
+++ taplist/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDphVewpM06PiOekI75YjBxzxhhyuUv1IA",
+    apiKey: "${{ secrets.APIKey }}",
     authDomain: "taplist-7a2bc.firebaseapp.com",
     databaseURL: "https://taplist-7a2bc.firebaseio.com",
     projectId: "taplist-7a2bc",
     storageBucket: "taplist-7a2bc.appspot.com",
     messagingSenderId: "52282594692",
-    appId: "1:52282594692:web:5ef7b67e0d2bdb9ad1b9d1"
+    appId: "${{ secrets.APPId }}"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
